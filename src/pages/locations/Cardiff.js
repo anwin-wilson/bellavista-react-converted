9462a1177136725fe82511fbc3dc52c2303b3dd6
anwin wilson <anwinws@gmail.com>
2025-12-05 01:31:36 +0530

    css chnages

--- src/pages/locations/Cardiff.js
+++ src/pages/locations/Cardiff.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../../styles/cardiff.css';
 
 const Cardiff = () => {
   const [heroExpanded, setHeroExpanded] = useState(false);
--- src/pages/locations/CollegeFields.js
+++ src/pages/locations/CollegeFields.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../../styles/cardiff.css';
 
 const CollegeFields = () => {
   const [heroExpanded, setHeroExpanded] = useState(false);
